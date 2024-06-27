a8ba004eae78f292a7c380227253fc4cb6cc1d85
みずまんま <app.mizumizurice@gmail.com>
2024-06-27 21:15:15 +0900

    final commit

--- src/pages/api/exit-preview.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiHandler } from "next";
-
-const exitPreview: NextApiHandler = (req, res) => {
-  res.clearPreviewData();
-  res.writeHead(307, { Location: "/" });
-  res.end();
-};
-
-export default exitPreview;
